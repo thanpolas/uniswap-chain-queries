7f91037ac4c9b43ef2e81fed06e32e8c41bab8ff
Thanos Polychronakis <thanpolas@gmail.com>
2021-08-21 21:06:13 +0300

    refactor uni V2 factory fetcher

--- src/uniswapv2/logic/factory-uniswapv2.ent.js
+++ src/uniswapv2/logic/factory-uniswapv2.ent.js
@@ -5,26 +5,22 @@
 
 const { ethers } = require('ethers');
 
-const { getProvider } = require('../../../ether.service');
-const log = require('../../../../../services/log.service').get();
-const { NOT_FOUND } = require('../../../constants/common-tokens.const');
+const factoryAbi = require('../abi/uniswap-v2-factory.abi.json');
+const { NOT_FOUND } = require('../../constants/address.const');
 
 const entity = (module.exports = {});
 
 /**
  * Queries a factory for existing LPs using a token pair for UniswapV2 clones.
  *
- * @param {Object} factory The factory constant object.
- * @param {Object} network An item from the network constants.
- * @param {Array<Object>} tokenPair The token pair to look for.
- * @return {Promise<string|void>} A promise with liquidity pool or empty.
+ * @param {string} factoryAddress The factory address.
+ * @param {Object} provider The provider to use.
+ * @param {Array<string>} tokenPair Array tuple with addresses of the token
+ *    pair to look for.
+ * @return {Promise<string|void>} A promise with liquidity pool address or empty.
  */
-entity.queryFactoryForLPuniswapv2 = async (factory, network, tokenPair) => {
-  const provider = getProvider(network);
-
-  const abi = factory.abi.abiFactory;
-
-  const contract = new ethers.Contract(factory.address, abi, provider);
+entity.queryUniV2FactoryForLP = async (factoryAddress, provider, tokenPair) => {
+  const contract = new ethers.Contract(factoryAddress, factoryAbi, provider);
 
   const [token0, token1] = tokenPair;
 
@@ -32,28 +28,12 @@ entity.queryFactoryForLPuniswapv2 = async (factory, network, tokenPair) => {
   try {
     lpAddress = await contract.getPair(token0.address, token1.address);
   } catch (ex) {
-    await log.error(
-      `queryFactoryForLPuniswapv2() Querying contract failed. Network` +
-        ` "${network.name}" Factory: "${factory.exchangeName}"` +
-        ` Token0:` +
-        ` ${token0.symbol} - ${token0.address} Token1: ${token1.symbol} -` +
-        ` ${token1.address}`,
-      {
-        error: ex,
-        relay: true,
-      },
-    );
     return;
   }
 
   if (lpAddress === NOT_FOUND) {
-    return [];
+    return;
   }
 
-  return [
-    {
-      address: lpAddress,
-      factory,
-    },
-  ];
+  return lpAddress;
 };
