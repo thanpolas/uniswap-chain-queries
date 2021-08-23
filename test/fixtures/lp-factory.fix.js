e6ef4259e688d9565e6c974ccb086558dcce0868
Thanos Polychronakis <thanpolas@gmail.com>
2021-08-23 15:11:12 +0300

    create the lpReservesFix fixture

--- test/fixtures/lp-factory.fix.js
+++ test/fixtures/lp-factory.fix.js
@@ -9,3 +9,10 @@ fix.factoryAddressFix = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f';
 
 // Uniswap V2 DAI/wETH LP
 fix.lpAddressFix = '0xa478c2975ab1ea89e8196811f51a7b7ade33eb11';
+
+fix.lpReservesFix = () => {
+  return {
+    _reserve0: '40696198400047034005278149',
+    _reserve1: '12231192365489141357156',
+  };
+};
