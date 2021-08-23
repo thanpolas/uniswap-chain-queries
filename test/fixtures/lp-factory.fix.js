b17641f49ea698f4cb3c1b098fc70763c8777600
Thanos Polychronakis <thanpolas@gmail.com>
2021-08-23 13:56:40 +0300

    create the lp and factory fixtures

---
+++ test/fixtures/lp-factory.fix.js
@@ -0,0 +1,11 @@
+/**
+ * @fileoverview Liquidity Pools and Factories fixtures.
+ */
+
+const fix = (module.exports = {});
+
+// Uniswap V2 mainnet
+fix.factoryAddressFix = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f';
+
+// Uniswap V2 DAI/wETH LP
+fix.lpAddressFix = '0xa478c2975ab1ea89e8196811f51a7b7ade33eb11';
