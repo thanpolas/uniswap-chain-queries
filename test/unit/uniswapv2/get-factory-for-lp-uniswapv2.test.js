4bc314661d37e0b6cbbe7e47c595756111868c53
Thanos Polychronakis <thanpolas@gmail.com>
2021-08-23 14:00:59 +0300

    add empty result from univ2 factory query

--- test/unit/uniswapv2/get-factory-for-lp-uniswapv2.test.js
+++ test/unit/uniswapv2/get-factory-for-lp-uniswapv2.test.js
@@ -35,4 +35,24 @@ describe('queryFactoryForLPuniswapv2()', () => {
       expect(res[0]).toEqual(lpAddressFix);
     });
   });
+  it('should return empty array when LP not found', async () => {
+    const provMock = providerMock();
+    const contractMock = contractFactoryUniV2();
+
+    contractMock.getPair.mockResolvedValue(
+      '0x0000000000000000000000000000000000000000',
+    );
+    contractProvider.getFactoryContract = contractMock.Contract;
+
+    const { provider } = provMock;
+
+    const res = await queryFactoryForLPuniswapv2(
+      factoryAddressFix,
+      provider,
+      tokenPair(),
+    );
+
+    expect(res).toBeArray();
+    expect(res).toHaveLength(0);
+  });
 });
