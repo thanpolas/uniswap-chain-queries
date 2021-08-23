af0899096755d796502b4072272f8a644d7e3d05
Thanos Polychronakis <thanpolas@gmail.com>
2021-08-23 12:17:45 +0300

    implement the first test case for getLPTokensData()

---
+++ test/unit/get-lp-tokens-data.test.js
@@ -0,0 +1,41 @@
+/**
+ * @fileoverview Test the getLPTokensData() method.
+ */
+
+const { ethers } = require('ethers');
+
+const { getLPTokensData } = require('../..');
+
+const { lpContractMock } = require('../setup/lp-contract.mock');
+const { providerMock } = require('../setup/provider.mock');
+const { contractToken } = require('../setup/ethers.mock');
+
+const {
+  assert: assertLpTokensData,
+} = require('../assert/lp-token-data.assert');
+const { assert: assertTokenData } = require('../assert/token-data.assert');
+const { token0Data, token1Data } = require('../fixtures/token.fix');
+
+describe('getLPTokensData()', () => {
+  describe('Happy Path', () => {
+    it('should return expected result', async () => {
+      const lpMock = lpContractMock();
+      const provMock = providerMock();
+      const contractMock = contractToken();
+
+      ethers.Contract = contractMock.Contract;
+
+      const { lpContract } = lpMock;
+      const { provider } = provMock;
+
+      const res = await getLPTokensData(lpContract, provider);
+
+      assertLpTokensData(res);
+
+      const [token0, token1] = res;
+
+      assertTokenData(token0, token0Data());
+      assertTokenData(token1, token1Data());
+    });
+  });
+});
