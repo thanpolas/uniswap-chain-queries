acfce287a7b3fc225eeaed116a899df9bc2e4dd5
Thanos Polychronakis <thanpolas@gmail.com>
2021-08-23 12:17:34 +0300

    create the provider mock

---
+++ test/setup/provider.mock.js
@@ -0,0 +1,17 @@
+/**
+ * @fileoverview Produces an ether.js provider mock.
+ */
+
+const { providerNetworkFix } = require('../fixtures/provider.fix');
+
+const mock = (module.exports = {});
+
+mock.providerMock = () => {
+  const getNetwork = jest.fn(async () => providerNetworkFix());
+  const provider = jest.fn();
+
+  return {
+    provider,
+    getNetwork,
+  };
+};
