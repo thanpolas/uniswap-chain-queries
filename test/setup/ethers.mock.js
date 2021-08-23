65f85393efaa0a08b9b97e9411a0cd1e776ca2f1
Thanos Polychronakis <thanpolas@gmail.com>
2021-08-23 12:43:41 +0300

    fix undefined mocks of token contract mock

--- test/setup/ethers.mock.js
+++ test/setup/ethers.mock.js
@@ -16,17 +16,13 @@ const mock = (module.exports = {});
  * @return {Object}
  */
 mock.contractToken = () => {
-  let decimals;
-  let name;
-  let symbol;
+  let tokenData;
+  const decimals = jest.fn(async () => tokenData.decimals);
+  const name = jest.fn(async () => tokenData.name);
+  const symbol = jest.fn(async () => tokenData.symbol);
 
   const Contract = jest.fn((tokenAddress) => {
-    const tokenData =
-      tokenAddress === token0Address ? token0Data() : token1Data();
-
-    decimals = jest.fn(async () => tokenData.decimals);
-    name = jest.fn(async () => tokenData.name);
-    symbol = jest.fn(async () => tokenData.symbol);
+    tokenData = tokenAddress === token0Address ? token0Data() : token1Data();
 
     return {
       decimals,
