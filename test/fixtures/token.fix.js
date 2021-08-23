611a0facc86dc5121cb4aa2da8f3c2068e9195fc
Thanos Polychronakis <thanpolas@gmail.com>
2021-08-23 11:30:50 +0300

    create token fixtures

---
+++ test/fixtures/token.fix.js
@@ -0,0 +1,8 @@
+/**
+ * @fileoverview Token related fixtures.
+ */
+
+const fix = (module.exports = {});
+
+fix.token0Address = '0x6B175474E89094C44Da98b954EedeAC495271d0F';
+fix.token1Address = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2';
