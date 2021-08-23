b11b0c76b043b244b9085059e09e42927eddf749
Thanos Polychronakis <thanpolas@gmail.com>
2021-08-23 11:30:58 +0300

    create lp-contract mock

---
+++ test/setup/lp-contract.mock.js
@@ -0,0 +1,23 @@
+/**
+ * @fileoverview Produces a Liquidity Pool ether.js contract mock.
+ */
+
+const { token0Address, token1Address } = require('../fixtures/token.fix');
+
+const mock = (module.exports = {});
+
+mock.getLPContractMock = () => {
+  const token0 = jest.fn(() => Promise.resolve(token0Address));
+  const token1 = jest.fn(() => Promise.resolve(token1Address));
+
+  const lpContract = jest.fn({
+    token0,
+    token1,
+  });
+
+  return {
+    token0,
+    token1,
+    lpContract,
+  };
+};
