629be2617eb23784c060c92adcce25d342b648a3
Thanos Polychronakis <thanpolas@gmail.com>
2021-08-23 16:37:21 +0300

    create v3 factory test

---
+++ test/unit/uniswapv3/get-factory-for-lp-uniswapv3.test.js
@@ -0,0 +1,46 @@
+/**
+ * @fileoverview Test the queryFactoryForLPuniswapv3() method.
+ */
+
+const { queryFactoryForLPuniswapv3 } = require('../../..');
+const contractProviderUniv3 = require('../../../src/uniswapv3/logic/contract-provider.ent');
+
+const { providerMock } = require('../../setup/provider.mock');
+const { tokenPair } = require('../../fixtures/token.fix');
+const { contractFactoryUniV3 } = require('../../setup/contracts.mock');
+
+const {
+  factoryAddressV3Fix,
+  lpAddressV3SmallFeeFix,
+  lpAddressV3MediumFeeFix,
+  lpAddressV3HighFeeFix,
+} = require('../../fixtures/lp-factory.fix');
+
+describe('queryFactoryForLPuniswapv3()', () => {
+  describe('Happy Path', () => {
+    it('should return expected result', async () => {
+      const provMock = providerMock();
+      const contractMock = contractFactoryUniV3();
+
+      contractProviderUniv3.getFactoryContract = contractMock.Contract;
+
+      const { provider } = provMock;
+
+      const res = await queryFactoryForLPuniswapv3(
+        factoryAddressV3Fix,
+        provider,
+        tokenPair(),
+      );
+
+      expect(res).toBeArray();
+      expect(res).toHaveLength(3);
+
+      const allPools = [
+        lpAddressV3SmallFeeFix,
+        lpAddressV3MediumFeeFix,
+        lpAddressV3HighFeeFix,
+      ];
+      expect(res).toIncludeAllMembers(allPools);
+    });
+  });
+});
