ce468d1440dd38860de6b04b7877cc30a93859c9
Thanos Polychronakis <thanpolas@gmail.com>
2021-08-23 13:02:09 +0300

    create tests for getLPTokenDecimals()

---
+++ test/unit/get-lp-token-decimals.test.js
@@ -0,0 +1,57 @@
+/**
+ * @fileoverview Test the getLPTokensData() method.
+ */
+
+const { ethers } = require('ethers');
+
+const { getLPTokenDecimals } = require('../..');
+const contractProvider = require('../../src/erc20tokens/logic/contract-provider');
+
+const { lpContractMock } = require('../setup/lp-contract.mock');
+const { providerMock } = require('../setup/provider.mock');
+const { contractToken } = require('../setup/ethers.mock');
+
+describe('getLPTokenDecimals()', () => {
+  describe('Happy Path', () => {
+    it('should return expected result', async () => {
+      const lpMock = lpContractMock();
+      const provMock = providerMock();
+      const contractMock = contractToken();
+
+      contractProvider.getERC20Contract = contractMock.Contract;
+      ethers.Contract = contractMock.Contract;
+
+      const { lpContract } = lpMock;
+      const { provider } = provMock;
+
+      const res = await getLPTokenDecimals(lpContract, provider);
+
+      const [token0Decimals, token1Decimals] = res;
+      expect(token0Decimals).toEqual(18);
+      expect(token1Decimals).toEqual(18);
+    });
+    it('should return passed decimals and not fetch', async () => {
+      const lpMock = lpContractMock();
+      const provMock = providerMock();
+      const contractMock = contractToken();
+
+      contractProvider.getERC20Contract = contractMock.Contract;
+      ethers.Contract = contractMock.Contract;
+
+      const { lpContract } = lpMock;
+      const { provider } = provMock;
+
+      const decimals = [18, 18];
+
+      const res = await getLPTokenDecimals(lpContract, provider, decimals);
+
+      const [token0Decimals, token1Decimals] = res;
+      expect(token0Decimals).toEqual(18);
+      expect(token1Decimals).toEqual(18);
+
+      expect(contractMock.decimals).toHaveReturnedTimes(0);
+      expect(contractMock.name).toHaveReturnedTimes(0);
+      expect(contractMock.symbol).toHaveReturnedTimes(0);
+    });
+  });
+});
